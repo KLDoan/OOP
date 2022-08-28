c25fe9f30acaca7edfc00a4796c0d11901928b11
unknown <longquydoan2020@gmail.com>
2022-08-28 21:58:48 +0700

    adam eva

--- AandE.js
+++ AandE.js
@@ -1,15 +1,46 @@
-class apple {
-    constructor (){
-        this.weight = 10 
+class Apple {
+    constructor() {
+        this.weight = 10
     }
-    getWeight(){
+    getWeight() {
         return this.weight;
     }
 }
-class human {
-    constructor(name,gender,weight){
-        this.name =  name 
-        this.gender =  gender 
-        this.weight = weight
+const apple = new Apple()
+var appleWeight = apple.getWeight()
+//human
+class Human {
+    constructor(name, gender) {
+        this.name = name
+        this.gender = gender
+        this.weight = 0
+    }
+    getName() {
+        return this.name
+    }
+    getGender() {
+        return this.gender;
+    }
+    talk() {
+        console.log("Hello tao la ")
+    }
+    eat() {
+        for (let i = 0; i < appleWeight; i++) {
+            apple.getWeight -= 1;
+            this.weight += 1;
+            return this.weight;
+        }
+    }
+    getHumanWeight() {
+        return this.weight;
+    }
+    checkApple() {
+        return appleWeight;
     }
 }
+const human = new Human("Adam", "male");
+let human_name = human.getName();
+let human_gender = human.getGender();
+let eatApple = human.eat();
+let human_weight = human.getHumanWeight();
+let checkAppleweight = human.checkApple();
